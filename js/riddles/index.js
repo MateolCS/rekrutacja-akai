36dfc3c20e6d8ca25afd871730508a28d975b9fd
MateoICS <mateusz.namysl0318@gmail.com>
2023-10-26 15:16:35 +0200

    riddles: implemented my solution for given task

--- js/riddles/index.js
+++ js/riddles/index.js
@@ -1,7 +1,7 @@
 // 1. odwróć liczbę
 // np dla 12345, funkcja powinna zwrócić 54321
-function reverseNumber() {
-  // uzupełnij tutaj
+function reverseNumber(inNumber) {
+  return +inNumber.toString().split("").reverse().join("");
 }
 
 console.log("1.", reverseNumber(12345));
@@ -10,7 +10,12 @@ console.log("1.", reverseNumber(12345));
 // dla tablicy tab powinniśmy otrzymać 2 + 4 + 6 + 8 = 20
 const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function addEven(array) {
-  // uzupełnij tutaj
+  return array.reduce((acc, curr) => {
+    if (curr % 2 === 0) {
+      return acc + curr;
+    }
+    return acc;
+  }, 0);
 }
 
 console.log("2.", addEven(tab));
